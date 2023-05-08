3c705705274421594d6a7cdc00b62017949d63c5
Una <una.gvozdenov@adacta-fintech.com>
2023-05-08 14:15:38 +0200

    added login-page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
 import { Component } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormGroup,
-  Validators
-} from '@angular/forms';
-import Validation from './utils/validation';
 
 @Component({
   selector: 'my-app',
@@ -13,55 +6,5 @@ import Validation from './utils/validation';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  form: FormGroup;
-  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group(
-      {
-        fullname: ['', Validators.required],
-        username: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(20)
-          ]
-        ],
-        email: ['', [Validators.required, Validators.email]],
-        password: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(6),
-            Validators.maxLength(40)
-          ]
-        ],
-        confirmPassword: ['', Validators.required],
-        acceptTerms: [false, Validators.requiredTrue]
-      },
-      {
-        validators: [Validation.match('password', 'confirmPassword')]
-      }
-    );
-  }
-
-  get f(): { [key: string]: AbstractControl } {
-    return this.form.controls;
-  }
-
-  onSubmit(): void {
-    this.submitted = true;
-
-    if (this.form.invalid) {
-      return;
-    }
-
-    console.log(JSON.stringify(this.form.value, null, 2));
-  }
-
-  onReset(): void {
-    this.submitted = false;
-    this.form.reset();
-  }
 }
